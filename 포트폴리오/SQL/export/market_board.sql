679fad92f2f02ac0ca68e471878a3f401c0b8445
rosangels2 <rosangels2@naver.com>
2019-09-22 15:05:27 +0900

    상품 등록 완료 후 DB export

--- 포트폴리오/SQL/export/market_board.sql
+++ 포트폴리오/SQL/export/market_board.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `market` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 USE `market`;
--- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: market
 -- ------------------------------------------------------
@@ -9,7 +9,7 @@ USE `market`;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,14 +23,14 @@ USE `market`;
 
 DROP TABLE IF EXISTS `board`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `board` (
   `no` int(11) NOT NULL AUTO_INCREMENT,
   `category` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `board_no` int(11) DEFAULT NULL,
   `item_no` int(11) DEFAULT NULL,
   `writer` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `to` varchar(12) COLLATE utf8_bin DEFAULT NULL,
+  `to` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `title` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `contents` longtext CHARACTER SET utf8 COLLATE utf8_bin,
   `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -61,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-20 17:16:55
+-- Dump completed on 2019-09-22 15:05:01
--- 포트폴리오/SQL/export/market_buy.sql
+++ 포트폴리오/SQL/export/market_buy.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `market` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 USE `market`;
--- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: market
 -- ------------------------------------------------------
@@ -9,7 +9,7 @@ USE `market`;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,12 +23,12 @@ USE `market`;
 
 DROP TABLE IF EXISTS `buy`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `buy` (
   `no` int(11) NOT NULL AUTO_INCREMENT,
   `id` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `item_no` int(11) NOT NULL,
-  `seller_id` varchar(12) COLLATE utf8_bin DEFAULT NULL,
+  `seller_id` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `option_no` int(11) DEFAULT NULL,
   `select` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `detail` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
@@ -67,4 +67,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-20 17:16:53
+-- Dump completed on 2019-09-22 15:04:59
